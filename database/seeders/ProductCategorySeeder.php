f5cd7eb68238d0c04425d07ade3a14a1f05c6ce8
Eduardo Mendoza Campos <emendoza9507@gmail.com>
2024-04-10 07:08:45 -0400

    First Commit and Inizilize the project

---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProductCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [ 'Bebidas', 'Confituras', 'Otros' ];
+
+        foreach($categories as $category) {
+            ProductCategory::create(['category_name' => $category]);
+        }
+    }
+}
