9087f412755d24826eb9e785a59583b7a7035c47
Eduardo Mendoza Campos <emendoza9507@gmail.com>
2024-04-18 21:15:29 -0400

    Workin on render

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,3 +1,9 @@
+chmod -R ugo+rw /.composer
+chown -R $USER:www-data /var/www/html/storage/
+chown -R $USER:www-data /var/www/html/bootstrap/cache/
+chmod -R 775 /var/www/html/storage/
+chmod -R 775 /var/www/html/bootstrap/cache/
+
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
